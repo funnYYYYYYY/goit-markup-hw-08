7da54537da902eb6e4ef6e5d7536e4d9290a4be5
Pavlo Myliutyn <pasam9300@gmail.com>
2022-05-17 20:42:50 +0200

    done Header

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.open-menu'),
+    closeMenuBtn: document.querySelector('.menu-closed'),
+    menu: document.querySelector('.mob-menu'),
+    // body: document.querySelector('.body'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    // refs.body.classList.toggle('no-scroll');
+  }
+})();
